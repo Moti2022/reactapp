6faaba4af44dc0897adeef71f5832ca9d35bf163
EliasRaisanen <eliasraisanen1@gmail.com>
2022-11-10 12:25:44 +0200

    kyselyt näkyvät

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import SeeQuery from './components/seeQuery';
 
 function App() {
   return (
     <div className="App">
-    
+    <SeeQuery/>
     </div>
   );
 }
--- src/components/seeQuery.js
+++ src/components/seeQuery.js
@@ -1,4 +1,48 @@
+
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 
 
+function SeeQueries() {
+    const [query, setQueries] = useState([]);
+   
+    
+
+
+    useEffect( () => fetchData(), []) 
+       const fetchData = () => {
+        fetch('https://moti2022.herokuapp.com/queries')
+        .then(vastaus => vastaus.json())
+        .then(vastausData => {
+            setQueries(vastausData);
+            console.log(vastausData);
+            
+
+
+        }) 
+       
+        
+    };
+    
+    return (
+        
+        <div>
+          <table>
+            <tbody>
+                {
+                    query.map((query) => (
+                        <tr key = {query.query_id}>
+                            <td>{query.heading}</td> 
+                            <td>{query.description}</td> 
+                        </tr>
+                    ))}
+                
+            </tbody>
+          </table>
+        </div>
+        
+    )}
+    export default SeeQueries
+        
+ 
